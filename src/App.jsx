f871b757bd4d89ae285de2d4eaf009e75ae73e59
Suman-1997 <ny619619@gmail.com>
2024-10-15 12:18:39 +0530

    Tailwind CSS configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-
+import React,{ useState,useEffect } from "react"
+import {useDispatch} from "react-redux"
+import authService from "./appwrite/auth"
+import {login,logout} from "./store/authSlice"
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_ID)
-  return (
-    <>
-      <h1>A Blog Project with appwrite</h1>
-    </>
-  )
+  const [loading,setLoading] = useState(false)
+  const dispatch = useDispatch()
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if (userData) {
+        dispatch(login({userData}))
+      } else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false))
+  },[])
+  return !loading ? (
+    <div class></div>
+  ) : null
 }
 
 export default App
