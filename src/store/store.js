79138b1abc40f744353cbcd6ad3940f413964b1a
Suman-1997 <ny619619@gmail.com>
2024-10-18 07:27:52 +0530

    redux toolkit

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit"
+import authSlice from "./authSlice"
 
 const store = configureStore({
-    reducers:{
-
+    reducer: {
+        auth : authSlice,
+        //TODO: add more slices here for posts
     }
-})
+});
 
 export default store
