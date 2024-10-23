39e8dd8b86ed2ae4747bb1852918afe93f478146
Suman-1997 <ny619619@gmail.com>
2024-10-23 15:25:17 +0530

    Logo Component

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,8 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import container from "./container/container";
 
-export {Header,
+export {
+        Header,
         Footer,
         container
     } //Exporting components via this index file 
