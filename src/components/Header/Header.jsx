648efbfaeac5e3079c341f370fe0a7fd788ca3d8
Suman-1997 <ny619619@gmail.com>
2024-10-23 16:10:06 +0530

    learnde about useNavigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,42 @@
 import React from 'react'
+import {Container,Logo,LogoutBtn} from "../index"
+import { useNavigation } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 function Header() {
+  const authStatus = useSelector((state)=>state.auth.status)
+  const navigate = useNavigation()
+
+  const navItems = [
+    {
+      name: 'Home',
+      slug: "/",
+      active: true
+    }, 
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+    },
+    {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+    },
+    {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+    },
+    {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+    },
+  ]
+
+
   return (
     <div>
       Header
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,14 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import container from "./container/container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn"
+
 
 export {
         Header,
         Footer,
-        container
+        container,
+        Logo,
+        LogoutBtn
     } //Exporting components via this index file 
