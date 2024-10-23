3306576ec1be34de021dcc2f4f25deb116e55ed4
Suman-1997 <ny619619@gmail.com>
2024-10-23 15:15:43 +0530

    container component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div>
-      Footer //Design footer 
+      Footer 
     </div>
   )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,8 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import container from "./container/container";
 
-export {Header,Footer} //Exporting the Header and footer components 
+export {Header,
+        Footer,
+        container
+    } //Exporting the Header and footer components 
